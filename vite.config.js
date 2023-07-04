9eef2e94921ff8c9ca2b5f94a73908f10297bc93
PaulinaST <paustrzy@gmail.com>
2023-07-04 21:51:30 +0200

    config file update

--- vite.config.js
+++ vite.config.js
@@ -14,17 +14,17 @@ export default defineConfig({
       output: {
         chunkFileNames: "assets/js/[name]-[hash].js",
         entryFileNames: "assets/js/[name]-[hash].js",
-        assetFileNames: ({ name, ext }) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(ext)) {
-            return `assets/images/[name]-[hash][ext]`;
-          }
+        // assetFileNames: ({ name, ext }) => {
+        //   if (/\.(gif|jpe?g|png|svg)$/.test(ext)) {
+        //     return `assets/images/[name]-[hash][ext]`;
+        //   }
 
-          if (ext === ".css") {
-            return `assets/css/[name]-[hash][ext]`;
-          }
+        //   if (ext === ".css") {
+        //     return `assets/css/[name]-[hash][ext]`;
+        //   }
 
-          return `assets/[name]-[hash][ext]`;
-        },
+        //   return `assets/[name]-[hash][ext]`;
+        // },
       },
     },
   },
